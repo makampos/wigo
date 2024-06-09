5e529a9efe04e24f30d81475d7184d773c4be080
makampos <matheus.campos@edu.unifil.br>
2024-06-09 15:36:00 -0300

    feat: Create API controllers
    - Added BeneficiaryController to manage beneficiary-related endpoints.
    - Added UsersController to manage user-related endpoints.
    - Added MediatR dependencies
    - Added Docker-compose

--- src/Wigo.API/Program.cs
+++ src/Wigo.API/Program.cs
@@ -1,3 +1,11 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Wigo.Domain.Interfaces;
+using Wigo.Infrastructure.Data;
+using Wigo.Infrastructure.Interfaces;
+using Wigo.Infrastructure.Repositories;
+using Wigo.Service.Handlers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +13,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IBeneficiaryRepository, BeneficiaryRepository>();
+
+builder.Services.AddMediatR(typeof(Program).Assembly,
+    typeof(AddUserCommandHandler).Assembly,
+    typeof(AddBeneficiaryCommandHandler).Assembly,
+    typeof(GetBeneficiariesByUserIdQueryHandler).Assembly);
 
 var app = builder.Build();
 
